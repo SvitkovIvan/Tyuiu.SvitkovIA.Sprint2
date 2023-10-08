d3d41c67777a582fa1e235b8aaef4c1c086ebaae
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-10-08 23:49:31 +0500

    Task5

---
+++ Tyuiu.SvitkovIA.Sprint2.Task5.V7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SvitkovIA.Sprint2.Task5.V7.Lib;
+
+namespace Tyuiu.SvitkovIA.Sprint2.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Свитков И. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант 7                                                               *");
+            Console.WriteLine("* Выполнил: Свитков Иван Андреевич | АСОиУб-23-2                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch, вычисляет       *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("* Условие: С начала 1990 года по некоторый день прошло n месяцев и 2 дня. *");
+            Console.WriteLine("* Определить название месяца (январь, февраль и т. п.) этого дня.         *");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Укажите с какого года ведется отсчёт: ");
+            int startYear = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Укажите сколько месяцев прошло со 2 января этого года: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ: *");
+            Console.WriteLine("***************************************************************************");
+
+            string resultMonthName = ds.FindMonthName(startYear, n);
+            Console.WriteLine($"С начала {startYear} года прошло " + n + " месяцев");
+            Console.WriteLine("Тогда сейчас " + resultMonthName);
+            Console.ReadKey();
+        }
+    }
+}
+
+       
